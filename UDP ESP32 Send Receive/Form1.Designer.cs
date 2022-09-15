a8de8c450e90e1a80d2e0c37084b25bd2747a0ff
mzaldirizaldi <mzaldirizaldi@gmail.com>
2022-09-15 15:37:53 +0700

    Add files via upload

---
+++ UDP ESP32 Send Receive/Form1.Designer.cs
@@ -0,0 +1,174 @@
+﻿
+namespace UDP_ESP32_Send_Receive
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.button_send = new System.Windows.Forms.Button();
+            this.textBox_message = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.textBox_ipAddress = new System.Windows.Forms.TextBox();
+            this.textBox_port = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.groupBox2 = new System.Windows.Forms.GroupBox();
+            this.listBox_received = new System.Windows.Forms.ListBox();
+            this.groupBox1.SuspendLayout();
+            this.groupBox2.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.label3);
+            this.groupBox1.Controls.Add(this.button_send);
+            this.groupBox1.Controls.Add(this.textBox_message);
+            this.groupBox1.Controls.Add(this.label1);
+            this.groupBox1.Controls.Add(this.textBox_ipAddress);
+            this.groupBox1.Controls.Add(this.textBox_port);
+            this.groupBox1.Controls.Add(this.label2);
+            this.groupBox1.Location = new System.Drawing.Point(12, 12);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(776, 269);
+            this.groupBox1.TabIndex = 11;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "Send";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(6, 102);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(65, 17);
+            this.label3.TabIndex = 11;
+            this.label3.Text = "Message";
+            // 
+            // button_send
+            // 
+            this.button_send.Location = new System.Drawing.Point(9, 233);
+            this.button_send.Name = "button_send";
+            this.button_send.Size = new System.Drawing.Size(93, 30);
+            this.button_send.TabIndex = 10;
+            this.button_send.Text = "Send";
+            this.button_send.UseVisualStyleBackColor = true;
+            this.button_send.Click += new System.EventHandler(this.button_send_Click);
+            // 
+            // textBox_message
+            // 
+            this.textBox_message.Location = new System.Drawing.Point(9, 123);
+            this.textBox_message.Margin = new System.Windows.Forms.Padding(4);
+            this.textBox_message.Multiline = true;
+            this.textBox_message.Name = "textBox_message";
+            this.textBox_message.Size = new System.Drawing.Size(760, 106);
+            this.textBox_message.TabIndex = 9;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(6, 40);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(76, 17);
+            this.label1.TabIndex = 5;
+            this.label1.Text = "IP Address";
+            // 
+            // textBox_ipAddress
+            // 
+            this.textBox_ipAddress.Location = new System.Drawing.Point(99, 40);
+            this.textBox_ipAddress.Name = "textBox_ipAddress";
+            this.textBox_ipAddress.Size = new System.Drawing.Size(170, 22);
+            this.textBox_ipAddress.TabIndex = 6;
+            // 
+            // textBox_port
+            // 
+            this.textBox_port.Location = new System.Drawing.Point(99, 68);
+            this.textBox_port.Name = "textBox_port";
+            this.textBox_port.Size = new System.Drawing.Size(170, 22);
+            this.textBox_port.TabIndex = 8;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(6, 68);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(34, 17);
+            this.label2.TabIndex = 7;
+            this.label2.Text = "Port";
+            // 
+            // groupBox2
+            // 
+            this.groupBox2.Controls.Add(this.listBox_received);
+            this.groupBox2.Location = new System.Drawing.Point(13, 288);
+            this.groupBox2.Name = "groupBox2";
+            this.groupBox2.Size = new System.Drawing.Size(776, 179);
+            this.groupBox2.TabIndex = 12;
+            this.groupBox2.TabStop = false;
+            this.groupBox2.Text = "Receive";
+            // 
+            // listBox_received
+            // 
+            this.listBox_received.FormattingEnabled = true;
+            this.listBox_received.ItemHeight = 16;
+            this.listBox_received.Location = new System.Drawing.Point(8, 22);
+            this.listBox_received.Margin = new System.Windows.Forms.Padding(4);
+            this.listBox_received.Name = "listBox_received";
+            this.listBox_received.Size = new System.Drawing.Size(761, 148);
+            this.listBox_received.TabIndex = 3;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 478);
+            this.Controls.Add(this.groupBox2);
+            this.Controls.Add(this.groupBox1);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.Name = "Form1";
+            this.Text = "UDP ESP32 Send Receive";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox1.PerformLayout();
+            this.groupBox2.ResumeLayout(false);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox textBox_ipAddress;
+        private System.Windows.Forms.TextBox textBox_port;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Button button_send;
+        private System.Windows.Forms.TextBox textBox_message;
+        private System.Windows.Forms.GroupBox groupBox2;
+        private System.Windows.Forms.ListBox listBox_received;
+    }
+}
+
---
+++ UDP ESP32 Send Receive/Form1.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Threading;
+using System.Net;
+using System.Net.Sockets;
+
+namespace UDP_ESP32_Send_Receive
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button_send_Click(object sender, EventArgs e)
+        {
+            UdpClient udpClient = new UdpClient();
+            udpClient.Connect(textBox_ipAddress.Text, Convert.ToInt16(textBox_port.Text));
+            Byte[] sendData = Encoding.ASCII.GetBytes(textBox_message.Text);
+            udpClient.Send(sendData, sendData.Length);
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            Thread thdUDPServer = new Thread(new ThreadStart(serverThread));
+            thdUDPServer.Start();
+        }
+
+        public void serverThread()
+        {
+            UdpClient udpClient = new UdpClient(8080);
+            while (true)
+            {
+                IPEndPoint RemoteIpEndPoint = new IPEndPoint(IPAddress.Any, 8080);
+                Byte[] receiveBytes = udpClient.Receive(ref RemoteIpEndPoint);
+                string returnData = Encoding.ASCII.GetString(receiveBytes);
+
+                this.Invoke(new MethodInvoker(delegate ()
+                {
+
+                    listBox_received.Items.Add(RemoteIpEndPoint.Address.ToString() + " : " + DateTime.Now.ToString("yyyy-MM-dd hh:mm:ss") + " : " + returnData.ToString());
+                    listBox_received.SelectedIndex = listBox_received.Items.Count - 1;
+                    listBox_received.SelectedIndex = -1;
+
+                }));
+            }
+
+
+        }
+    }
+}
